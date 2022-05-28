911a71c4c76d177646824998d91b2a6359ca90a8
epicowl555 <rein.dyla.2020@ksz.edu-zg.ch>
2022-05-28 16:20:06 +0200

    Added Java Class for Blocks

---
+++ src/main/java/com/epicowl55/mccourse/block/ModBlocks.java
@@ -0,0 +1,40 @@
+package com.epicowl55.mccourse.block;
+
+import com.epicowl55.mccourse.MCCourseMod;
+import com.epicowl55.mccourse.item.ModItems;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, MCCourseMod.MOD_ID);
+
+    public static final RegistryObject<Block> COBALT_BLOCK = registerBlock("cobalt_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.METAL)
+                    .strength(5f).requiresCorrectToolForDrops()),CreativeModeTab.TAB_BUILDING_BLOCKS);
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab){
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn, tab);
+        return toReturn;
+    }
+
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block, CreativeModeTab tab){
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
+                new Item.Properties().tab(tab)));
+    }
+
+    public static void  register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+}
